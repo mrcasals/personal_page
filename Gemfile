2bd0137796cef70be7215d7ef4f013587c08ac0c
Marc Riera <mrc2407@gmail.com>
2012-01-15 18:11:22 +0100

    First commit: Rails app with ActiveAdmin

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'sqlite3'
+gem 'activeadmin', git: 'git://github.com/gregbell/active_admin.git'
+gem 'sass-rails'
+gem 'meta_search',    '>= 1.1.0.pre'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'turn', '~> 0.8.3', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalPage::Application.config.secret_token = '4e5cd41402e0e9b36a02ae4a49f00bd65c308b1901e160a91eb64a74e6c9810a48d5db8cb2d986d6b19766b5d85c54e87096924cc8459346e02fcbe521b1f77c'

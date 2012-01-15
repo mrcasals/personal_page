bf3dadde4c6c1e740d19ebf4a9230be8fa8db664
Marc Riera <mrc2407@gmail.com>
2012-01-15 18:47:28 +0100

    Write spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,26 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
+require 'mocha'
+require 'machinist/active_record'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :transaction
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+ActiveRecord::Base.observers = []
 
-  # Add more helper methods to be used by all tests here...
+class MiniTest::Unit::TestCase
+  include Mocha::API
+  def setup
+    Machinist.reset_before_test
+    DatabaseCleaner.start
+    mocha_setup
+  end
+  def teardown
+    DatabaseCleaner.clean
+    mocha_teardown
+  end
 end
+
+require File.expand_path(File.dirname(__FILE__) + '/blueprints')
+require 'minitest/autorun'

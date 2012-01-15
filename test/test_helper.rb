3ff5410f331f9a35416b356931ad8d08f93f9a6a
Marc Riera <mrc2407@gmail.com>
2012-01-15 18:58:51 +0100

    Remove mocha references

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/spec'
-require 'mocha'
 require 'machinist/active_record'
 require 'database_cleaner'
 DatabaseCleaner.strategy = :transaction
@@ -10,15 +9,12 @@ DatabaseCleaner.strategy = :transaction
 ActiveRecord::Base.observers = []
 
 class MiniTest::Unit::TestCase
-  include Mocha::API
   def setup
     Machinist.reset_before_test
     DatabaseCleaner.start
-    mocha_setup
   end
   def teardown
     DatabaseCleaner.clean
-    mocha_teardown
   end
 end
 

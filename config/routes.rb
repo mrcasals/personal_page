7cf807ad46b536b0ed4d18ed85376704ed10818a
Marc Riera <mrc2407@gmail.com>
2012-02-02 20:17:06 +0100

    Add post routes for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PersonalPage::Application.routes.draw do
   ActiveAdmin.routes(self)
 
+  resources :posts, only: [:index, :show]
+
   devise_for :users, ActiveAdmin::Devise.config
 
   root :to => 'posts#index'
